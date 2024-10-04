f368490cc8e0b3661beff28301902e2ee547846d
Ismael Diego <ismaeldiegoqm@gmail.com>
2024-10-04 11:44:46 -0500

    Login

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
     <meta charset="utf-8" />
@@ -18,62 +18,42 @@
     <link href="style/assets/css/theme.min.css" rel="stylesheet" type="text/css" />
 
 </head>
-
+<?php 
+session_start();
+if (!empty($_SESSION['us_tipo'])) {
+  header("Location: Controller/LoginController.php");
+}
+else{
+  session_destroy();
+?>
 <body>
- 
     <div>
         <div class="container">
-            <div class="row">
-                <div class="col-12">
+            <div class="row ">
+                <div class="col-2"></div>
+                <div class="col-8">
                     <div class="d-flex align-items-center min-vh-100">
                         <div class="w-100 d-block bg-white shadow-lg rounded my-5">
                             <div class="row">
-                                <div class="col-lg-5 d-none d-lg-block bg-login rounded-left"></div>
-                                <div class="col-lg-7">
+                                <div class="col-lg-12">
                                     <div class="p-5">
                                         <div class="text-center mb-5">
                                             <a href="index.html" class="text-dark font-size-22 font-family-secondary">
-                                                <i class="mdi mdi-alpha-x-circle"></i> <b>XELORO</b>
+                                                <i class="mdi mdi-alpha-x-circle"></i> <b>PANEL DE ACCESO</b>
                                             </a>
                                         </div>
-                                        <h1 class="h5 mb-1">Welcome Back!</h1>
-                                        <p class="text-muted mb-4">Enter your email address and password to access admin panel.</p>
-                                        <form class="user">
+                                        <h1 class="h5 mb-1">¡BIENVENIDO DE NUEVO!</h1>
+                                        <p class="text-muted mb-4">Ingrese sus credenciales para acceder al panel de administración.</p>
+                                        <form class="user" action="Controller/LoginController.php" method="post">
                                             <div class="form-group">
-                                                <input type="email" class="form-control form-control-user" id="exampleInputEmail" placeholder="Email Address">
+                                                <input type="text" class="form-control form-control-user" id="exampleInputEmail" name="nombre_usu" placeholder="Usuario">
                                             </div>
                                             <div class="form-group">
-                                                <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
+                                                <input type="password" class="form-control form-control-user" id="exampleInputPassword" name="password_usu" placeholder="Contraseña">
                                             </div>
-                                            <a href="" class="btn btn-success btn-block waves-effect waves-light"> Log In </a>
-
-                                            <div class="text-center mt-4">
-                                                <h5 class="text-muted font-size-16">Sign in using</h5>
-                                            
-                                                <ul class="list-inline mt-3 mb-0">
-                                                    <li class="list-inline-item">
-                                                        <a href="javascript: void(0);" class="social-list-item border-primary text-primary"><i class="mdi mdi-facebook"></i></a>
-                                                    </li>
-                                                    <li class="list-inline-item">
-                                                        <a href="javascript: void(0);" class="social-list-item border-danger text-danger"><i class="mdi mdi-google"></i></a>
-                                                    </li>
-                                                    <li class="list-inline-item">
-                                                        <a href="javascript: void(0);" class="social-list-item border-info text-info"><i class="mdi mdi-twitter"></i></a>
-                                                    </li>
-                                                    <li class="list-inline-item">
-                                                        <a href="javascript: void(0);" class="social-list-item border-secondary text-secondary"><i class="mdi mdi-github-circle"></i></a>
-                                                    </li>
-                                                </ul>
-                                            </div>
-                                            
+                                            <button type="submit" class="btn btn-success btn-block waves-effect waves-light">Sign In</button>
                                         </form>
 
-                                        <div class="row mt-4">
-                                            <div class="col-12 text-center">
-                                                <p class="text-muted mb-2"><a href="pages-recoverpw.html" class="text-muted font-weight-medium ml-1">Forgot your password?</a></p>
-                                                <p class="text-muted mb-0">Don't have an account? <a href="pages-register.html" class="text-muted font-weight-medium ml-1"><b>Sign Up</b></a></p>
-                                            </div> <!-- end col -->
-                                        </div>
                                         <!-- end row -->
                                     </div> <!-- end .padding-5 -->
                                 </div> <!-- end col -->
@@ -98,5 +78,8 @@
     <script src="style/assets/js/theme.js"></script>
 
 </body>
-
 </html>
+<?php 
+
+}
+?>
