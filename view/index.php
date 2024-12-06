3feb29ec9e4e140383230dbd75ffaba2561ddbfa
Ismael Diego <ismaeldiegoqm@gmail.com>
2024-12-06 06:15:15 -0500

    dasboard

--- view/index.php
+++ view/index.php
@@ -158,10 +158,276 @@ if (!isset($_SESSION['S_IDUSUARIO'])) { //si existe
             <!-- /.content-header -->
 
             <!-- Main content -->
-            <div class="content">
+    <section class="content">
+      <div class="container-fluid">
+        <!-- Info boxes -->
+        <div class="row">
+          <div class="col-12 col-sm-6 col-md-3">
+            <div class="info-box">
+              <span class="info-box-icon bg-info elevation-1"><i class="fas fa-list"></i></span>
 
+              <div class="info-box-content">
+                <span class="info-box-text">Registros <b class="text-info">Totales</b></span>
+                <span class="info-box-number">
+                  10
+                </span>
+              </div>
+              <!-- /.info-box-content -->
             </div>
-            <!-- /.content -->
+            <!-- /.info-box -->
+          </div>
+          <!-- /.col -->
+          <div class="col-12 col-sm-6 col-md-3">
+            <div class="info-box mb-3">
+              <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-users"></i></span>
+
+              <div class="info-box-content">
+                <span class="info-box-text">Pacientes <b class="text-danger">pendiente de intervención</b></span>
+                <span class="info-box-number">41,410</span>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+          <!-- /.col -->
+
+          <!-- fix for small devices only -->
+          <div class="clearfix hidden-md-up"></div>
+
+          <div class="col-12 col-sm-6 col-md-3">
+            <div class="info-box mb-3">
+              <span class="info-box-icon bg-success elevation-1"><i class="fas fa-users"></i></span>
+
+              <div class="info-box-content">
+              <span class="info-box-text">Pacientes <b class="text-success">en alta</b></span>
+
+                <span class="info-box-number">760</span>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+          <!-- /.col -->
+          <div class="col-12 col-sm-6 col-md-3">
+            <div class="info-box mb-3">
+              <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-users"></i></span>
+
+              <div class="info-box-content">
+              <span class="info-box-text">Pacientes <b class="text-warning">en tratamiento</b></span>
+
+                <span class="info-box-number">2,000</span>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+          <!-- /.col -->
+        </div>
+        <!-- /.row -->
+
+    
+
+        <!-- Main row -->
+        <div class="row">
+          <!-- Left col -->
+          <div class="col-md-6">
+                       <!-- TABLE: LATEST ORDERS -->
+                       <div class="card">
+              <div class="card-header border-transparent bg-danger" >
+                <h3 class="card-title">Lista de pacientes <b>pendiente de intervención</b></h3>
+
+                <div class="card-tools">
+                  <button type="button" class="btn btn-tool text-light" data-card-widget="collapse">
+                    <i class="fas fa-minus"></i>
+                  </button>
+                </div>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body p-0">
+                <div class="table-responsive">
+                  <table class="table m-0">
+                    <thead>
+                    <tr>
+                      <th>Order ID</th>
+                      <th>Pciente</th>
+                      <th>Estado</th>
+                      <th>Acción</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr class="bg-danger">
+                      <td><a href="pages/examples/invoice.html">OR9842</a></td>
+                      <td>Call of Duty IV</td>
+                      <td><span class="badge badge-success">Shipped</span></td>
+                      <td>
+                        <div class="sparkbar" data-color="#00a65a" data-height="20">90,80,90,-70,61,-83,63</div>
+                      </td>
+                    </tr>
+                   
+                    </tbody>
+                  </table>
+                </div>
+                <!-- /.table-responsive -->
+              </div>
+              <!-- /.card-body -->
+              <div class="card-footer clearfix">
+                <a href="javascript:void(0)" class="btn btn-sm btn-info float-right">Ver lista completa</a>
+              </div>
+              <!-- /.card-footer -->
+            </div>
+            <!-- /.card -->
+          
+
+
+          </div>
+          <!-- /.col -->
+
+          <div class="col-md-6">
+          <div class="card">
+              <div class="card-header border-transparent bg-success" >
+                <h3 class="card-title">Lista de pacientes <b>pendiente de intervención</b></h3>
+
+                <div class="card-tools">
+                  <button type="button" class="btn btn-tool text-light" data-card-widget="collapse">
+                    <i class="fas fa-minus"></i>
+                  </button>
+                </div>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body p-0">
+                <div class="table-responsive">
+                  <table class="table m-0">
+                    <thead>
+                    <tr>
+                      <th>Order ID</th>
+                      <th>Pciente</th>
+                      <th>Estado</th>
+                      <th>Acción</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr class="bg-success">
+                      <td><a href="pages/examples/invoice.html">OR9842</a></td>
+                      <td>Call of Duty IV</td>
+                      <td><span class="badge badge-success">Shipped</span></td>
+                      <td>
+                        <div class="sparkbar" data-color="#00a65a" data-height="20">90,80,90,-70,61,-83,63</div>
+                      </td>
+                    </tr>
+                    
+                    </tbody>
+                  </table>
+                </div>
+                <!-- /.table-responsive -->
+              </div>
+              <!-- /.card-body -->
+              <div class="card-footer clearfix">
+                <a href="javascript:void(0)" class="btn btn-sm btn-info float-right">Ver lista completa</a>
+              </div>
+              <!-- /.card-footer -->
+            </div>
+            <!-- /.card -->
+
+          </div>
+          <!-- /.col -->
+        </div>
+        <!-- /.row -->
+
+         <!-- Main row -->
+        <div class="row">
+          <!-- Left col -->
+          <div class="col-md-6">
+                       <!-- TABLE: LATEST ORDERS -->
+         <div class="card">
+              <div class="card-header border-transparent bg-warning" >
+                <h3 class="card-title">Lista de pacientes <b>en tratamiento</b></h3>
+
+                <div class="card-tools">
+                  <button type="button" class="btn btn-tool text-dark" data-card-widget="collapse">
+                    <i class="fas fa-minus"></i>
+                  </button>
+                </div>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body p-0">
+                <div class="table-responsive">
+                  <table class="table m-0">
+                    <thead>
+                    <tr>
+                      <th>Order ID</th>
+                      <th>Pciente</th>
+                      <th>Estado</th>
+                      <th>Acción</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr class="bg-warning">
+                      <td><a href="pages/examples/invoice.html">OR9842</a></td>
+                      <td>Call of Duty IV</td>
+                      <td><span class="badge badge-success">Shipped</span></td>
+                      <td>
+                        <div class="sparkbar" data-color="#00a65a" data-height="20">90,80,90,-70,61,-83,63</div>
+                      </td>
+                    </tr>
+                   
+                    </tbody>
+                  </table>
+                </div>
+                <!-- /.table-responsive -->
+              </div>
+              <!-- /.card-body -->
+              <div class="card-footer clearfix">
+                <a href="javascript:void(0)" class="btn btn-sm btn-info float-right">Ver lista completa</a>
+              </div>
+              <!-- /.card-footer -->
+            </div>
+            <!-- /.card -->
+          
+
+
+          </div>
+          <!-- /.col -->
+
+          <div class="col-md-6">
+            <!-- Info Boxes Style 2 -->
+            <div class="info-box mb-3 bg-warning">
+              <span class="info-box-icon"><i class="fas fa-users"></i></span>
+
+              <div class="info-box-content">
+                <span class="info-box-text"><b>EN TRATAMINENTO</b></span>
+                <span class="info-box-number">5,200</span>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+            <div class="info-box mb-3 bg-success">
+              <span class="info-box-icon"><i class="fas fa-users"></i></span>
+
+              <div class="info-box-content">
+                <span class="info-box-text"><b>EN ALTA</b></span>
+                <span class="info-box-number">92,050</span>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+            <div class="info-box mb-3 bg-danger">
+              <span class="info-box-icon"><i class="fas fa-users"></i></span>
+
+              <div class="info-box-content">
+                <span class="info-box-text"><b>PENDIENTE A INTERVENCIÓN</b></span>
+                <span class="info-box-number">114,381</span>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+
+          </div>
+          <!-- /.col -->
+        </div>
+        <!-- /.row -->
+      </div><!--/. container-fluid -->
+    </section>
+    <!-- /.content -->
         </div>
         <!-- /.content-wrapper -->
 
